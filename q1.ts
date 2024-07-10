c18e1a4c8bb07b8ca8024600eed73bfc04247745
Rafffan <vanidrisraffan@gmail.com>
2024-07-10 19:58:08 +0700

    finish

--- q1.ts
+++ q1.ts
@@ -1,18 +1,20 @@
-// const axios = require("axios");
 import axios from "axios";
 
-/* assign interface/type to the function definition properly */
 const getUser = async (userId: number) => {
-  /* Your code here */
+  try{
+    const res = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
+    const users = res.data;
+    return users.name;
+    
+  } catch(err){
+    return 'INVALID USER ID';
+  }
 };
 
-//test case
 const input1 = 1;
 const input2 = 100;
 
-//run
 getUser(input1).then((result) => console.log(result));
 getUser(input2).then((result) => console.log(result));
 
-// module.exports = getUser;
 export default getUser;
